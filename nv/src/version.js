180004c9d04173a397660a3216ec3ffa1ba35668
Callum <callum.morrison@mac.com>
2022-01-04 12:54:51 +0000

    Fix version bug for new folder structure

--- nv/src/version.js
+++ nv/src/version.js
@@ -1,5 +1,9 @@
 import fs from "fs";
+
 // Get version from package.json
-export const __version__ = JSON.parse(
-    fs.readFileSync(new URL("../../package.json", import.meta.url))
-).version;
+export const __version__ = () => {
+    console.log("Getting version number...");
+    return JSON.parse(
+        fs.readFileSync(new URL("../package.json", import.meta.url))
+    ).version;
+};
