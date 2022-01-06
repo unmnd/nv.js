6cf538c541df566497cb60a398b0ec8fcd85f157
XDGFX <callum.morrison@mac.com>
2022-01-06 14:56:37 +0000

    Improve method of getting version number

--- nv/src/version.js
+++ nv/src/version.js
@@ -1,10 +1,4 @@
-const fs = require("fs");
-const path = require("path");
-
-// Read the package.json file
-const packageJson = JSON.parse(
-    fs.readFileSync(path.join(__dirname, "..", "package.json"), "utf8")
-);
-const __version__ = packageJson.version;
+const package = require("../package.json");
+const __version__ = package.version;
 
 exports.__version__ = __version__;
