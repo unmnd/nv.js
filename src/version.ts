99bd47c0eba2fad0f11fef842b3a4d2051b1f3c1
XDGFX <callum.morrison@mac.com>
2024-04-11 15:14:41 +0100

    Bugfix for file path

--- src/version.ts
+++ src/version.ts
@@ -1,5 +1,11 @@
 import { readFileSync } from "fs";
 
 export const __version__ = JSON.parse(
-    readFileSync(__dirname + "/../package.json", "utf8"),
+    readFileSync(
+        __dirname +
+            (process.env.NODE_ENV === "production"
+                ? "/../../package.json"
+                : "/../package.json"),
+        "utf8",
+    ),
 ).version;
