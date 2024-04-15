c054e51ebfe5a145da128debf9bf3e927cdebc25
XDGFX <callum.morrison@mac.com>
2024-04-15 12:35:13 +0100

    Fix version selection

--- src/version.ts
+++ src/version.ts
@@ -1,1 +1,14 @@
-export const __version__ = process.env.npm_package_version;
+import { readFileSync } from "fs";
+import { join } from "path";
+
+let version: string | undefined;
+try {
+    version = JSON.parse(
+        readFileSync(join(__dirname, "..", "..", "package.json"), "utf8"),
+    ).version;
+} catch (e) {
+    version = JSON.parse(
+        readFileSync(join(__dirname, "..", "package.json"), "utf8"),
+    ).version;
+}
+export const __version__ = version;
